fa049f59cff3e530dd8db21c0f942b39ef3b0e8d
Vanessa Paixão Cortes <vanessastangherlin@gmail.com>
2021-10-15 15:50:32 -0300

    Implementação da adição do produto no carrinho.

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className, innerText, eventListener=null) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (eventListener){
+    e.addEventListener('click', eventListener);
+  }
   return e;
 }
 
@@ -19,7 +22,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!', productClickListener));
 
   return section;
 }
@@ -40,19 +43,36 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+//Função de adicionar item no carrinho
+function productClickListener(event) {
+  //createCartItemElement();
+  const sku = getSkuFromProductItem(event.target.parentElement);
+  const product = findBySKU(sku);
+  console.log(product);
+  const item = createCartItemElement(product[0]);
+  let lista = document.getElementsByClassName("cart__items");
+  lista[0].appendChild(item);
+}
+
 //Funções para busca de dados e organização
 
 async function returnProducts(){
   let response = await fetch ("https://api.mercadolibre.com/sites/MLB/search?q=computador")
   let data = await response.json();
-  let results =[]
+  let results =[];
   data.results.forEach(element=>{results.push({sku:element.id,name:element.title,image:element.thumbnail,salePrice:element.price})})
-  return results
+  return results;
 }
 
+function findBySKU(sku) {
+  return resultado.filter( x => x.sku == sku);
+}
+
+let resultado = [];
+
 window.onload = async () => {
-  let resultado = await returnProducts();
-  let lista = document.getElementsByClassName("items")
+  resultado = await returnProducts();
+  let lista = document.getElementsByClassName("items");
   resultado.forEach(element => {
     let objeto = createProductItemElement(element);
     lista[0].appendChild(objeto);
